3607fe1b8c0ab0879f24ed07ab4ddd8ad01505e3
Sam Yang <sam9883043@gmail.com>
2025-06-06 13:11:55 +0900

    feat: first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+# app = Flask(__name__)
+
+
+# @app.route("/")
+# def main():
+#     return f"Hello, World!"
+
+
+def main():
+    print("Hello, World!")
+
+
+if __name__ == "__main__":
+    main()
+    # app.run(debug=True)
